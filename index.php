b89453bd17b9faa98fdbb8392a67b76cfa6c6e75
Julie <juliejarocewicz@gmail.com>
2018-09-23 18:21:34 -0400

    added index page and changes to login

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Baker Classifieds</title>
+    <link href = "lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel = "stylesheet">
+      <link href="css/styles.css" rel="stylesheet">
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+      <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+<?php
+    include "nav.php";
+?>
+<div class="container">
+    <h4>Items for sale:</h4>
+    <?php
+        // Prod
+        // $servername = "108.179.220.92";
+        // $username = "dbljtwon_root";
+        // $password = "j6T2&^7eR7";
+        // $mydb = "dbljtwon_php";
+
+        // $mysqli = new mysqli($servername, $username, $password, $mydb);
+
+        // Local
+        $servername="127.0.0.1";
+        $port=3307;
+        $username="root";
+        $password="root";
+        $mydb="classifieds";
+
+        $mysqli = new mysqli($servername, $username, $password, $port, $mydb);
+
+        // Check connection
+        if (mysqli_connect_error()) {
+            printf("Connection to the quote database failed, please try again: " . mysqli_connect_error());
+            exit();
+        } 
+
+        if ($stmt = $mysqli->prepare("SELECT * FROM classifieds.products;")) {
+                $stmt->execute();
+                $stmt->bind_result($id, $title, $description, $image, $category, $price, $sold);
+                while ($stmt->fetch()) {
+                    echo "<div class='media'>
+                            <div class='media-left'>
+                            <a href='#'>
+                                <img class='media-object' src='" . $image . "alt='product for sale'>
+                            </a>
+                            </div>
+                            <div class='media-body'>
+                            <h4 class='media-heading'>" . $title . "</h4>
+                            <p>" . $description . "</p>
+                            <em>" . $category . "</em>
+                            <br />
+                            <p>" . $price . "</p>
+                            </div>
+                        </div>";
+                }
+            $stmt->close();
+        }
+        $mysqli->close();
+    ?>
+    </div>
+</body>
+</html>
--- login.php
+++ login.php
@@ -6,15 +6,39 @@
 <html lang = "en">
    
    <head>
-      <title>Tutorialspoint.com</title>
-      <link href = "lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel = "text/css">
+      <title>Baker Classifieds Login</title>
+      <link href = "lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel = "stylesheet">
       <link href="css/styles.css" rel="stylesheet">
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
       <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js" type="text/javascript"></script>
    </head>
 	
    <body>
       
-      <h2>Please Enter Username and Password</h2> 
+      <h3>Login to Baker Classifieds</h3> 
+      <img src="images/bc-logo_2017-1502808997-4638.jpg" alt="logo" class="img-responsive" id="logo">
+            
+      <div class = "container" id="login_fields">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <label for = "username">Username:</label>
+            <input type = "text" class = "form-control" 
+               name = "username"  
+               required autofocus></br>
+               <label for = "password">Password:</label>
+            <input type = "password" class = "form-control"
+               name = "password" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+         </form>
+			
+         <!-- Click here to clean <a href = "logout.php" tite = "Logout">Session. -->
+         
+      </div> 
+
       <div class = "container form-signin">
          
          <?php
@@ -23,38 +47,19 @@
             if (isset($_POST['login']) && !empty($_POST['username']) 
                && !empty($_POST['password'])) {
 				
-               if ($_POST['username'] == 'tutorialspoint' && 
+               if ($_POST['username'] == 'test' && 
                   $_POST['password'] == '1234') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'test';
                   
-                  echo 'You have entered valid use name and password';
+                  header('Location: index.php'); 
                }else {
                   $msg = 'Wrong username or password';
                }
             }
          ?>
       </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username = tutorialspoint" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
-         </form>
-			
-         Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
-      </div> 
       
    </body>
 </html>
