d16e0cde4bd3259ec338b0f4c4f9b512403505bf
kelly <kellyoetzel@gmail.com>
2018-10-04 21:36:38 -0400

    updated upload image function

--- new_listing.php
+++ new_listing.php
@@ -1,52 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <title>Classifieds Site - New Listing</title>
-    <link href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous"></script>
-    <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <title>Baker Classifieds - New Listing</title>
 </head>
-
 <body>
-    <?php
-        include "nav.php";
-    ?>
-    <div class="container">
-        <h1>Classifieds Site - New Item</h1>
+    <h1>Baker Classifieds - New Listing</h1>
 
-        <form action="list.php" method="post" enctype="multipart/form-data">
-            <table border="0">
-                <tr>
-                    <td>Title</td>
-                    <td><input type="text" name="title" maxlength="60" size="60" class="form-control"></td>
-                </tr>
-                <tr>
-                    <td>Description</td>
-                    <td> <input type="text" name="description" maxlength="150" size="150" class="form-control"></td>
-                </tr>
-                <tr>
-                    <td>Category</td>
-                    <td> <input type="text" name="Category" maxlength="30" size="30" class="form-control"></td>
-                </tr>
-                <tr>
-                    <td>Price $</td>
-                    <td><input type="text" name="price" maxlength="7" size="7" class="form-control"></td>
-                </tr>
-                <tr>
-                    <td>Select Image to Upload</td>
-                    <td><input type="file" name="image" id="image"></td>
-                </tr>
-                <tr><td><br/></td></tr>
-                <tr>
-                    <td colspan="2"><input type="submit" value="List For Sale"></td>
-                </tr>
-            </table>
-        </form>
-    </div>
+    <form action="list.php" method="post" enctype="multipart/form-data">
+		<p>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title">
+        </p>
+		
+		<p>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description">
+        </p>
+		
+		<p>
+            <label for="category">Category</label>
+            <input type="text" name="category" id="category">
+        </p>
+        
+        <p>
+            <label for="price">Price $</label>
+            <input type="text" name="price" id="price">
+        </p>
+		
+        <p>
+            <label for="image">Select Image to Upload</label>
+            <input type="file" name="image" id="image">
+			<input type="submit" value="Upload Image" name="submit">
+        </p>
+        
+		<input type="submit" value="Submit"></td>
+       
+    </form>
 </body>
-
 </html>
