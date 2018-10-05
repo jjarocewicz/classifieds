da3bc9fa659d7e17849d865e4014be36c304cb9c
kelly <kellyoetzel@gmail.com>
2018-10-04 23:03:25 -0400

    Updated form/image upload function

--- new_listing.php
+++ new_listing.php
@@ -8,34 +8,19 @@
     <h1>Baker Classifieds - New Listing</h1>
 
     <form action="list.php" method="post" enctype="multipart/form-data">
-		<p>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title">
-        </p>
-		
-		<p>
-            <label for="description">Description</label>
-            <input type="text" name="description" id="description">
-        </p>
-		
-		<p>
-            <label for="category">Category</label>
-            <input type="text" name="category" id="category">
-        </p>
-        
-        <p>
-            <label for="price">Price $</label>
-            <input type="text" name="price" id="price">
-        </p>
-		
-        <p>
-            <label for="image">Select Image to Upload</label>
-            <input type="file" name="image" id="image">
-			<input type="submit" value="Upload Image" name="submit">
-        </p>
-        
-		<input type="submit" value="Submit"></td>
-       
+		Title: <input type="text" name="title"><br>
+		<br>
+        Description: <input type="text" name="description"><br>
+		<br>
+        Category: <input type="text" name="category"><br>
+		<br>
+        Price $ <input type="text" name="price"><br>
+		<br>
+        Select Image to Upload: <input type="file" name="image"><br>
+		<br>
+		<input type="submit" value="Upload Image" name="submit"><br>
+		<br>
+		<input type="submit" value="Submit"><br>       
     </form>
 </body>
 </html>
