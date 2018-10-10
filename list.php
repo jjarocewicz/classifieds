f4e345c921eea46643233244ede943df59bdd5b2
kelly <kellyoetzel@gmail.com>
2018-10-09 22:17:30 -0400

    added link to CSS

--- list.php
+++ list.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Baker Classifieds - List Item</title>
 </head>
 
--- new_listing.php
+++ new_listing.php
@@ -2,25 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Baker Classifieds - New Listing</title>
 </head>
 <body>
     <h1>Baker Classifieds - New Listing</h1>
 
-    <form action="list.php" method="post" enctype="multipart/form-data">
-		Title: <input type="text" name="title"><br>
-		<br>
-        Description: <input type="text" name="description"><br>
-		<br>
-        Category: <input type="text" name="category"><br>
-		<br>
-        Price $ <input type="text" name="price"><br>
-		<br>
-        <!--Select Image to Upload: <input type="file" name="image"><br>
-		<br>
-		<input type="submit" value="Upload Image" name="submit"><br>-->
-		<br>
-		<input type="submit" value="Submit"><br>       
-    </form>
+    <form action="list.php" method="post">
+		<p>
+			<label for="idProducts">Product ID:</label>
+			<input type="text" name="product_id" id="idProducts">
+		</p>
+		<p>
+			<label for="title">Title:</label>
+			<input type="text" name="title" id="title">
+		</p>
+		<p>
+			<label for="description">Description:</label>
+			<input type="text" name="description" id="description">
+		</p>
+		<p>
+			<label for="category">Category:</label>
+			<input type="text" name="category" id="category">
+		</p>
+		<p>
+			<label for="price">Price:</label>
+			<input type="text" name="price" id="price">
+		</p>
+		<input type="submit" value="Submit">
+	</form>
 </body>
 </html>
